b98a78eb784662ae9185b783238cb514a59625b1
lilian017 <lilian.gitau211@gmail.com>
2023-02-21 04:07:24 -0800

    alphabet times 10" "

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase ten times.
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char l;
+	
+	int i;
+	
+	for (i = 0; i < 10 ; l++)
+	{
+		for (l = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(l);
+		}
+		_putchar('\n');
+	}
+}
