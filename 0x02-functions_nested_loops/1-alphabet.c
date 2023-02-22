c5e6aef74c7f0cea19d1d2085bcb3ba524f538ef
lilian017 <lilian.gitau211@gmail.com>
2023-02-22 14:07:30 -0800

    alphabet x10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,12 @@
 
 /**
  * main - A program that prints the lowercase alphabet
- *
+ * void
  * Return: Always 0
  */
 
 void print_alphabet(void)
+/** implementation code goes here*/
 {
 	char c;
 
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - Prints the alphabet in lowercase ten times.
+ * main - The function prints the alphabet in lowercase ten times.
  * followed by a new line
  */
 void print_alphabet_x10(void)
 {
 	char l;
 	int i;
-	
+
 	for (i = 0; i < 10 ; i++)
 	{
 		for (l = 'a' ; l <= 'z' ; l++)
