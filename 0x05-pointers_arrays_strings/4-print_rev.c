21a260c7e1567a18646f3af2149d131e3ebcf856
lilian017 <lilian.gitau211@gmail.com>
2023-02-27 23:46:27 -0800

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - minic the stdio version of puts
+ * @s: the value to process to the stdout in reverse.
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count--)
+	{
+		putchar(*(s + (count)));
+	}
+	putchar('\n');
+}
